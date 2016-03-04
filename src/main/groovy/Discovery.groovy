2daddb5c50d983d8e12c970e476140a876b02ea2
Hunter Scheib <hunterscheib@carfax.com>
2016-03-04 14:11:43 -0600

    using nashorn to execute a script

--- src/main/groovy/Discovery.groovy
+++ src/main/groovy/Discovery.groovy
@@ -4,23 +4,30 @@ import discovery.sonos.SonosDeviceFactory
 import discovery.sonos.SonosSystem
 import discovery.sonos.constants.SonosDeviceType
 
+import javax.script.ScriptEngine
+import javax.script.ScriptEngineManager
+
 class Discovery {
     public static void main(String... args) {
         println "Starting"
-        SonosSystem sonosSystem = new SonosDeviceFactory().getSonosSystem()
+//        SonosSystem sonosSystem = new SonosDeviceFactory().getSonosSystem()
+//
+//        if (sonosSystem.allDevices.size() > 0) {
+//            println "Sonos system found with ${sonosSystem.allDevices.size()} devices!"
+//        }
+//
+//        println sonosSystem.getDevicesByType(SonosDeviceType.PLAY3).first().hostname
+//
+//        println "Sub count : " + sonosSystem.getDevicesByType(SonosDeviceType.SUB).size()
+//        println "Play1 count : " + sonosSystem.getDevicesByType(SonosDeviceType.PLAY1).size()
+//        println "Play3 count : " + sonosSystem.getDevicesByType(SonosDeviceType.PLAY3).size()
+//        println "Play5 count : " + sonosSystem.getDevicesByType(SonosDeviceType.PLAY5).size()
+//        println "Playbar count : " + sonosSystem.getDevicesByType(SonosDeviceType.PLAYBAR).size()
+//        println "Bridge count : " + sonosSystem.getDevicesByType(SonosDeviceType.BRIDGE).size()
 
-        if (sonosSystem.allDevices.size() > 0) {
-            println "Sonos system found with ${sonosSystem.allDevices.size()} devices!"
-        }
+        ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+        engine.eval(new FileReader("src/main/groovy/discovery/script.js"));
 
-        sonosSystem.getDevicesByType(SonosDeviceType.PLAY3).first().getZoneAttributes()
-
-        println "Sub count : " + sonosSystem.getDevicesByType(SonosDeviceType.SUB).size()
-        println "Play1 count : " + sonosSystem.getDevicesByType(SonosDeviceType.PLAY1).size()
-        println "Play3 count : " + sonosSystem.getDevicesByType(SonosDeviceType.PLAY3).size()
-        println "Play5 count : " + sonosSystem.getDevicesByType(SonosDeviceType.PLAY5).size()
-        println "Playbar count : " + sonosSystem.getDevicesByType(SonosDeviceType.PLAYBAR).size()
-        println "Bridge count : " + sonosSystem.getDevicesByType(SonosDeviceType.BRIDGE).size()
         println "Finished"
     }
 }
